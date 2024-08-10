6797f0a1077b3e427ff2ad5ed2fb380ab1f0e6e1
Lans12345 <olanalans12345@gmail.com>
2024-08-10 10:21:09 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/firebase_options.dart';
 import 'package:mental_health_app/screens/auth/landing_screen.dart';
 import 'package:mental_health_app/screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'mindspace-73c00',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
