7e84a104ea62b2b8807855bf8e06d6df19b07874
Lans12345 <olanalans12345@gmail.com>
2024-08-12 08:12:29 +0800

    added chatbot

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,8 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mental_health_app/services/add_messages.dart';
 import 'package:mental_health_app/widgets/text_widget.dart';
-
+import 'package:ikchatbot/ikchatbot.dart';
 import '../services/api_service.dart';
+import '../utils/data.dart';
 
 class ChatScreen extends StatefulWidget {
   String time;
@@ -18,22 +19,49 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  final List<String> _messages = [];
-  final TextEditingController _textController = TextEditingController();
+  final chatBotConfig = IkChatBotConfig(
+    backgroundAssetimage: 'assets',
+    backgroundImage: 'assets',
+    useAsset: false,
+    //SMTP Rating to your mail Settings
+    ratingIconYes: const Icon(Icons.star),
+    ratingIconNo: const Icon(Icons.star_border),
+    ratingIconColor: Colors.black,
+    ratingBackgroundColor: Colors.white,
+    ratingButtonText: 'Submit Rating',
+    thankyouText: 'Thanks for your rating!',
+    ratingText: 'Rate your experience:',
+    ratingTitle: 'Thank you for using the chatbot!',
+    body: 'This is a test email sent from Flutter and Dart.',
+    subject: 'Test Rating',
+    recipient: 'recipient@example.com',
+    isSecure: false,
+    senderName: 'Your Name',
+    smtpUsername: 'Your Email',
+    smtpPassword: 'your password',
+    smtpServer: 'stmp.gmail.com',
+    smtpPort: 587,
+    //Settings to your system Configurations
+    sendIcon: const Icon(Icons.send, color: Colors.black),
+    userIcon: const Icon(Icons.person, color: Colors.white),
+    botIcon: const Icon(Icons.android, color: Colors.white),
+    botChatColor: const Color.fromARGB(255, 81, 80, 80),
+    delayBot: 100,
+    closingTime: 1,
+    delayResponse: 1,
+    userChatColor: Colors.blue,
+    waitingTime: 1,
+    keywords: keywords,
+    responses: responses,
+    backgroundColor: Colors.white,
 
-  void _handleSubmitted(String text) async {
-    if (text.isEmpty) return;
-
-    _textController.clear();
-    setState(() {
-      _messages.add('User: $text');
-    });
-
-    final String response = await ChatGPTService().getResponse(text);
-    setState(() {
-      _messages.add('ChatGPT: $response');
-    });
-  }
+    initialGreeting: "👋 Hello! \nHow can I assist you today?",
+    defaultResponse: "Sorry, I didn't understand your response.",
+    inactivityMessage: "Is there anything else you need help with?",
+    closingMessage: "This conversation will now close.",
+    inputHint: 'Send a message',
+    waitingText: 'Please wait...',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -42,39 +70,11 @@ class _ChatScreenState extends State<ChatScreen> {
           foregroundColor: Colors.white,
           backgroundColor: Colors.blue,
           title: TextWidget(
-            text: 'Chat',
+            text: 'Chatbot',
             fontSize: 18,
             color: Colors.white,
           ),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _messages.length,
-                itemBuilder: (context, index) =>
-                    ListTile(title: Text(_messages[index])),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _textController,
-                      decoration:
-                          const InputDecoration(hintText: 'Send a message'),
-                    ),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () => _handleSubmitted(_textController.text),
-                    child: const Icon(Icons.send),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ));
+        body: ikchatbot(config: chatBotConfig));
   }
 }
